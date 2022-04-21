cf16e68733dd0e39e4e56eef6c0e9bb19a18f832
narisanemo <104006296+narisanemo@users.noreply.github.com>
2022-04-21 19:38:32 +0400

    Update hacker.lua

--- hacker.lua
+++ hacker.lua
@@ -193,7 +193,16 @@ end)
 local ss = s:Tab("Animations")
 
 ss:Button("Animation Reset", function()
-	loadstring(game:HttpGet('hhttps://pastebin.com/raw/qvcYgWFD'))()
+	local Animate = game.Players.LocalPlayer.Character.Animate
+	Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=782841498"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=782841498"
+	Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+	Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=1083218792"
+	Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=1083439238"
+	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
+	game.Players.LocalPlayer.Character.Humanoid.Jump = true
+	wait(1)
 end)
 
 ss:Button("Astronaut", function()
